7f4298de64e5212d2a8d9fd750998f3b479d4e81
Rashid Al-Abri <itsrashidalabri@gmail.com>
2019-01-21 18:37:55 -0800

    New stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/testing/DriveDistanceTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.autonomous.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.autonomous.Auto;
+import org.firstinspires.ftc.teamcode.autonomous.Robot;
+import org.firstinspires.ftc.teamcode.utilities.GoldDetector;
+
+@Autonomous(name="[TEST] Drive Distance Test", group = "Tests")
+public class DriveDistanceTest extends LinearOpMode
+{
+    Robot robot;
+    Auto auto;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+        // Initialize objects
+        robot = new Robot(hardwareMap);
+        auto = new Auto(this, robot);
+
+        waitForStart();
+
+        auto.driveForDistance(5, 0.8);
+
+
+    }
+
+
+}
